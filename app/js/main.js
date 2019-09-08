f0d4c32bee2816e75a030e4fff1cd16c369d5041
Nienke van Poppel <nvpoppel@handpickedagencies.com>
2019-09-08 22:09:54 +0200

    finished detailpage, added overview page, cleaned up files

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,11 @@
-// var menu = document.getElementsByClassName('menuWrapper')[0];
-// var button = document.getElementsByClassName('button')[0];
-// var buttonLink = document.getElementsByClassName('btn-open')[0];
-
-// buttonLink.click(function() {
-//     menu.fadeToggle(200);
-//     buttonLink.toggleClass('btn-open').toggleClass('btn-close')
-// })
-
 $(document).ready(function(){
-    $(".button a").click(function(){
+    $(".toggle a").click(function(){
         $(".menuWrapper").fadeToggle(200);
-       $(this).toggleClass('btn-open').toggleClass('btn-close');
+       $(this).toggleClass('toggle-open').toggleClass('toggle-close');
     });
 });
 $('.overlay').on('click', function(){
     $(".menuWrapper").fadeToggle(200);   
-    $(".button a").toggleClass('btn-open').toggleClass('btn-close');
+    $(".toggle a").toggleClass('toggle-open').toggleClass('toggle-close');
     open = false;
 });
